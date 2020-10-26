1d2498857f36d1306ffe5afa0b377389eaa9c1e6
Radek Maňák <Radek.Manak@protonmail.com>
2020-10-26 16:06:12 +0100

    Test string literal scanning

--- apps/test_main.c
+++ apps/test_main.c
@@ -1,12 +1,12 @@
-#include "scanner.h"
+#include <scanner.h>
+#include <string.h>
 #include "testData.h"
 
 FILE* test_file = NULL;
 
 int setup(char * string){
-    if (test_file == NULL){
-        test_file = tmpfile();
-    }
+    init_test_data();
+    test_file = tmpfile();
     fprintf(test_file, "%s", string);
     rewind(test_file);
     if (set_file(test_file) == -1){
@@ -37,17 +37,78 @@ int test_no_errors(char * string){
     return 0;
 }
 
+int test_exact_token_sequence(char * string, Token* expected){
+    if (setup(string)){
+        fprintf(stderr, "Error setting up test environment");
+        return 1;
+    }
+    Token scanned;
+    for(int i = 0; next_token(&scanned); i++){
+        if (expected[i].token_type == scanned.token_type){
+            switch (expected[i].token_type) {
+                case TT_IDENTIFIER:
+                case TT_STRING_LITERAl:
+                    if (strcmp(expected[i].attribute.string, scanned.attribute.string) != 0){
+                        fprintf(stderr, "String \"%s\" does dot match \"%s\"", scanned.attribute.string, expected[i].attribute.string);
+                        return 1;
+                    }
+                    break;
+                case TT_KEYWORD:
+                    if (expected[i].attribute.keyword != scanned.attribute.keyword){
+                        fprintf(stderr, "Keyword %d does dot match %d", scanned.attribute.keyword, expected[i].attribute.keyword);
+                        return 1;
+                    }
+                    break;
+                case TT_INTEGER_LITERAL:
+                    if (expected[i].attribute.integer != scanned.attribute.integer){
+                        fprintf(stderr, "Integer attribute %ld does dot match %ld", scanned.attribute.integer, expected[i].attribute.integer);
+                        return 1;
+                    }
+                    break;
+                case TT_FLOATING_LITERAL:
+                    if (expected[i].attribute.floating != scanned.attribute.floating){
+                        fprintf(stderr, "Float %f does dot match %f", scanned.attribute.floating, expected[i].attribute.floating);
+                        return 1;
+                    }
+                    break;
+                default:
+                    break; // No attribute
+            }
+        } else {
+            fprintf(stderr, "Token type %d does not match %d", scanned.token_type, expected[i].token_type);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int print_tokens(char * string){
+    if (setup(string)){
+        fprintf(stderr, "Error setting up test environment");
+        return 1;
+    }
+    Token token;
+    while (next_token(&token)){
+        printf("%d - \"%s\"\n", token.token_type, to_string_attribute(&token));
+    }
+    return 0;
+}
+
 int main(){
     if(test_no_errors(TESTDATA_FACTORIAL_ITERATIVE)){
-        printf("TEST 1 FAILED");
+        printf("TEST 1 FAILED\n");
         return 1;
     }
     if(test_no_errors(TESTDATA_FACTORIAL_RECURSIVE)){
-        printf("TEST 2 FAILED");
+        printf("TEST 2 FAILED\n");
         return 1;
     }
     if(test_no_errors(TESTDATA_STRING_FUNCTION_EXAMPLE)){
-        printf("TEST 3 FAILED");
+        printf("TEST 3 FAILED\n");
+        return 1;
+    }
+    if (test_exact_token_sequence(TESTDATA_STRING_LITERALS, TESTDATA_STRING_LITERALS_TOKENS)){
+        printf("TEST 4 FAILED\n");
         return 1;
     }
     return 0;
