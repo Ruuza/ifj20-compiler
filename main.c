b050d2e7b11e81c72dcc40b2b02f6ba1f71c5649
Ruuza <RunyCZ@gmail.com>
2020-11-21 03:52:47 +0100

    update main.c

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
-int main(){
-    return 2;
+#include "parser.h"
+#include "scanner.h"
+
+int main(int argc, char const *argv[])
+{
+    FILE *srcFile;
+    char const *text = argv[1];
+    srcFile = fopen(argv[1], "r");
+
+    int scanner_file_return = set_file(srcFile);
+    if (scanner_file_return == -1)
+    {
+        printf("Failed to open file");
+        return 1;
+    }
+
+    int parse_return = parse();
+
+    return (parse_return);
 }
