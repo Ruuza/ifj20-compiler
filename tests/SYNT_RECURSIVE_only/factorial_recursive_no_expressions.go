beb214aae268ce7326767f2d64f7bee202e18f40
Ruuza <RunyCZ@gmail.com>
2020-11-23 02:18:33 +0100

    test added

---
+++ tests/SYNT_RECURSIVE_only/factorial_recursive_no_expressions.go
@@ -0,0 +1,28 @@
+package main
+func factorial(n int) (int) {
+    dec_n := 1
+    if 1 {
+    return 1
+} else {
+    tmp := 1
+    tmp = 1
+    return 1
+    }
+}
+func main() {
+    print(1)
+    a := 1
+    err := 1
+    a, err = 1
+if 1 {
+    if 1 {
+        print(1, 1)
+} else {
+    vysl := 1
+    vysl = 1
+print(1, 1, 1)
+}
+} else {
+    print(1)
+}
+}
